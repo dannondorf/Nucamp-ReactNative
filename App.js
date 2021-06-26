d7ea44d106a2fac5847414d5e90e0ea0bce3d12a
Daniel Brett <drummer.man.dan@icloud.com>
2021-06-26 12:04:23 -0500

    Activity Indicator

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+
+const store = ConfigureStore();
 
 export default function App() {
   return (
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+
---
+++ shared/baseUrl.js
@@ -0,0 +1,3 @@
+
+
+export const baseUrl = 'http://10.0.0.6:3001/';
